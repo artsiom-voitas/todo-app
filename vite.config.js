c141e5b4a3ef584b9580876b543d77e6314bc749
Artsiom Voitas <artsiom.voitas@gmail.com>
2023-06-27 18:32:56 +0300

    build: minor changes for first deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react()
-        // vitePluginFaviconsInject('./public/logo.svg')
-    ]
+    base: '/todo-app/',
+    plugins: [react(), vitePluginFaviconsInject('./public/logo.svg')]
 });
