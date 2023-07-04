1bc735c0d04e0db2f9692463dc3cba483cd40b6b
Artsiom Voitas <artsiom.voitas@gmail.com>
2023-07-04 21:31:23 +0300

    perf: added conditional rendering for TodoItems component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,15 @@
+import { useSelector } from 'react-redux';
 import CreateTodoField from '../CreateTodoField/';
 import TodoItems from '../TodoItems';
 
 function Home() {
+    const todos = useSelector((state) => state['todos']);
+
     return (
         <div className={'mb-20'}>
             <h1 className={'sm:text-2xl text-xl font-bold mb-10 text-center'}>Your To-Do List</h1>
             <CreateTodoField />
-            <TodoItems />
+            {todos.length >= 1 && <TodoItems />}
         </div>
     );
 }
